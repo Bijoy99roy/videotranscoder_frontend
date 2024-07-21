f225c396cd6049f453ad5d0f774966af0422b23c
Bijoy99roy <bijoy2014amp@gmail.com>
2024-07-21 21:36:31 +0530

    chore: Add console log

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -14,7 +14,7 @@ export function NavBar({isUploadPage=false, onUploadClick, handleChannel, onClic
     const navigate = useNavigate()
     const { user } = useContext(AuthContext);
 
-
+    console.log(user)
     return <>
     <div className="flex justify-between border-b-2 border-hidden my-2">
         <div className="flex flex-col justify-center ml-5 md:text-3xl font-mono cursor-pointer">
